3db151cc7c028a2cdbde125382b6a163343fdf57
sidhe3141 <j134a@aol.com>
2014-03-13 23:50:31 -0600

    Committing the entire project so far.

---
+++ sidhe3141utils.WordWrap/TextBox.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sidhe3141utils.WordWrap
+{
+    /// <summary>
+    /// A text box that wraps words and displays words character-by-character.
+    /// Author: James Yakura/sidhe3141.
+    /// </summary>
+    public class TextBox
+    {
+        char[] endLines = new char[]{' ', '\n', '-' };
+        string contents;
+        int width;
+        List<Word> parsedContents;
+
+        /// <summary>
+        /// Creates a text box that can handle word wrapping.
+        /// </summary>
+        /// <param name="contents">The text to be displayed.</param>
+        /// <param name="width">The number of characters that can fit on a line.</param>
+        public TextBox(string contents, int width)
+        {
+            this.width = width;
+            Contents = contents;
+        }
+        /// <summary>
+        /// Gets the portion of the text box that is displayed.
+        /// </summary>
+        public string DisplayedContents
+        {
+            get
+            {
+                string value = "";
+                string currentLine="";
+                foreach (Word x in parsedContents)
+                {
+                    if (currentLine.Length + x.dataDisplayed.Length > width)
+                    {
+                        value += currentLine+"\n";
+                        currentLine = "";
+                    }
+                    currentLine += x.dataDisplayed;
+                }
+                value += currentLine;
+                return value;
+            }
+        }
+
+        public string Contents
+        {
+            get
+            {
+                return contents;
+            }
+            set
+            {
+                contents = value;
+                parsedContents = new List<Word>();
+                string currentString = "";
+                for (int i = 0; i < contents.Length; i++)
+                {
+                    currentString += contents[i];
+                    bool endthisword = false;
+                    foreach (char x in endLines)
+                    {
+                        if (contents[i] == x | i==contents.Length-1)
+                        {
+                            endthisword = true;
+                        }
+                    }
+                    if (endthisword)
+                    {
+                        parsedContents.Add(new Word((string)currentString.Clone()));
+                        currentString = "";
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Completes the current word.
+        /// </summary>
+        public void CompleteWord()
+        {
+            foreach (Word x in parsedContents)
+            {
+                if (!x.WordComplete)
+                {
+                    x.advanceToEnd();
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Advances the current word.
+        /// </summary>
+        public void Advance()
+        {
+            foreach (Word x in parsedContents)
+            {
+                if (!x.WordComplete)
+                {
+                    x.Advance();
+                    break;
+                }
+            }
+        }
+        /// <summary>
+        /// Completes the text box.
+        /// </summary>
+        public void Complete()
+        {
+            foreach (Word x in parsedContents)
+            {
+                x.advanceToEnd();
+            }
+        }
+        /// <summary>
+        /// Gets a list of words in the text.
+        /// </summary>
+        public List<Word> ParsedContents
+        {
+            get
+            {
+                return parsedContents;
+            }
+        }
+
+    }
+}
---
+++ sidhe3141utils.WordWrap/Word.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sidhe3141utils.WordWrap
+{
+    /// <summary>
+    /// A word that can be displayed.
+    /// Author: James Yakura/sidhe3141.
+    /// </summary>
+    public class Word
+    {
+        string contents;
+        int charsDisplayed;
+        bool wordComplete;
+
+        /// <summary>
+        /// Creates a new word.
+        /// </summary>
+        /// <param name="contents">The string to be displayed.</param>
+        public Word(string contents)
+        {
+            this.contents = contents;
+            charsDisplayed = 0;
+            wordComplete = false;
+        }
+
+        /// <summary>
+        /// Gets the part of the word that is displayed.
+        /// </summary>
+        public string dataDisplayed
+        {
+            get
+            {
+                string result = "";
+                for (int i = 1; i <= charsDisplayed; i++)
+                {
+                    result += contents[i - 1];
+                }
+                return result;
+            }
+        }
+        /// <summary>
+        /// Gets whether the word is finished.
+        /// </summary>
+        public bool WordComplete
+        {
+            get
+            {
+                return wordComplete;
+            }
+        }
+
+        /// <summary>
+        /// Advances the word display by one character.
+        /// </summary>
+        public void Advance()
+        {
+            charsDisplayed++;
+            if (charsDisplayed >= contents.Length)
+            {
+                wordComplete = true;
+            }
+        }
+        /// <summary>
+        /// Advances the word display to its end.
+        /// </summary>
+        public void advanceToEnd()
+        {
+            while (!wordComplete)
+            {
+                Advance();
+            }
+        }
+    }
+}
